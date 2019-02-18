3574dcc514a75d5ae7433108322923b947b51d52
lordtailotr <47754467+lordtailotr@users.noreply.github.com>
2019-02-18 20:54:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,12 +78,6 @@ client.unload = command => {
   });
 };
 
-client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('Aleyküm Selam Knk Hoşgeldin');
-  }
-});
-
 client.elevation = message => {
   if(!message.guild) {
 	return; }
